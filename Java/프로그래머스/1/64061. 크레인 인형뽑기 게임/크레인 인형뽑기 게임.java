b230381448f9dc73717b40bfd7afe0515e458e89
kimsee8200 <144791985+kimsee8200@users.noreply.github.com>
2024-04-08 11:53:10 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.70 ms, Memory: 76.2 MB -BaekjoonHub

--- Java/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ Java/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -12,42 +12,38 @@ class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
         Stack<Integer> s = new Stack<>();
+        
+        //moves의 길이 만큼 반복.
         for(int move:moves){
-            
-            move--;
-            //System.out.println(move+"__");
+            move--; // 인덱스는 0부터 시작.
             int i=0;
-            if(board[board[move].length-1][move]==0){
+            
+            // 맨 끝이 0인지 판단. -> 그럼 모든 요소가 0
+            if(board[board[move].length-1][move]==0){ // 어차피 n*n 길이는 동일.
                 continue;
             }
-            // for(int[] b : board){
-            //     for(int c : b)
-            //         System.out.print(c+" ");
-            //     System.out.print("\n");
-            // }
+          
+            // 크레인을 내림. -> 요소가 있는 곳 까지 행을 내림.
             while(true){
-                //System.out.println(i);
-                //System.out.println(board[i][move]);
                 if(board[i][move]!=0){
                     break;
                 }
                 i++;
             }
             
-            
+            // 만약 스텍이 비어있지 않고, 스택의 peek과 뽑은 요소가 동일하다면,
+            // pop -> 0으로 바꿈.
             if(!s.empty()&&s.peek()==board[i][move]){
-                //System.out.println("dfe");
                 s.pop();
                 board[i][move] = 0;
-                //System.out.println(s);
+    
                 answer=answer+2;
-            }else{
-                //System.out.println(board[i][move]+"_");
+            }else{ // 아니면 요소를 집어넣고 0으로 바꿈.
                 s.push(board[i][move]);
                 board[i][move]=0;
-                //System.out.println(s);
             }
         }
+        
         return answer;
     }
 }
