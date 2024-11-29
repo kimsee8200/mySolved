48499679985e30d4c8317076cbfe57ed94e1d463
kimsee8200 <144791985+kimsee8200@users.noreply.github.com>
2024-11-29 13:29:51 +0900

    [level 0] Title: 옹알이 (1), Time: 4.66 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/README.md
+++ 프로그래머스/0/120956. 옹알이 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옹알이 (1) - 120956 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=java) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.70 ms
+메모리: 75.6 MB, 시간: 4.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 22:18:57
+2024년 11월 29일 13:29:49
 
 ### 문제 설명
 
--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,16 +1,27 @@
+// 스택 사용
+// 1. 스택에 각 단어를 넣음 -> 반복문 활용
+// 2. 만약 스택에서 확인 한 조합이 4가지 조건중 하나에 충족하면 pop
+// 3. 카운트를 증가 시킴.
+import java.util.*;
+
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        for(String baba:babbling){
-            baba=baba.replace("aya"," ");
-            baba=baba.replace("woo"," ");
-            baba=baba.replace("ye"," ");
-            baba=baba.replace("ma"," ");
-            baba=baba.replace(" ","");
-            if(baba.length()==0){
+        String temp = "";
+        for(String baby: babbling){
+            String[] cry = baby.split("");
+            for(String alpabet:cry){
+                temp += alpabet;
+                if(temp.equals("aya")||temp.equals("ye")
+                   ||temp.equals("woo")||temp.equals("ma")){
+                    temp = "";
+                }
+               
+            }
+            if(temp.equals("")){
                 answer++;
             }
-            System.out.println(baba);
+            temp = "";
         }
         return answer;
     }
