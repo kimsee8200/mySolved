e44e54a3302ea0c89e3bef4e682ccf37c1f0f0df
kimsee8200 <144791985+kimsee8200@users.noreply.github.com>
2024-11-29 16:11:56 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        int count = 1;
-        
-        
-        int i=0;
-        int j=0;
-        int b=0;
-        while(count<=(n*n)){
+        int count = 0;
+        int minuse = 0;
+        int i = 0;
+        int j = 0;
+        int b = 0;
+        while(count < n*n){
             count++;
             answer[i][j]=count;
-            System.out.print(i);
-           System.out.println(j);
             if(i==b&&j<n-(b+1)){
                 j++;
             }else if((j==n-(b+1)&&i<n-(b+1))){
                 i++;
             }else if(i==n-(b+1)&&j>b){
                 j--;
-                if(j==b){
-                    b++;
-                }
             }else if(j==b&&i>b){
                 i--;
             }
-            
+            if(i==b+1&&j==b){
+                b++;
+            }
         }
+        
         return answer;
     }
 }
+
+// 나선형 배열 
+// 1 2 3 4 -> 5 6 7 -> 8 9 10 -> 11 12 -> 13 14 -> 15 16
+// 1 2 3 4 5 -> 6 7 8 9 -> 10 11 12 13 -> 14 15 16 -> 17 18 19 -> 20 21 -> 22 23 -> 24 -> 25
